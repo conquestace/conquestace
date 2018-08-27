272f74d2b6f5481c1e63542e577fc3c5d3d34532
conquestace <ashiful.b@gmail.com>
2018-08-26 23:15:39 -0700

    Signed-off-by: conquestace <ashiful.b@gmail.com>

---
+++ start-page-master/js/main.js
@@ -0,0 +1,48 @@
+var config =
+{
+    "sites":
+    [
+        { "url": "https://boards.4chan.org/w/#catalog",                            "text": "/w/" },
+        { "url": "https://boards.4chan.org/a/#catalog",                             "text": "/a/"  },
+        { "url": "https://boards.4chan.org/g/#catalog",                             "text": "/g/"  },
+        { "url": "http://nibl.co.uk/bots.php",                                      "text": "XDCC" },
+        { "url": "https://my.trentu.ca/portal",                                          "text": "WORK"  },
+        { "url": "http://bakabt.me/",                                               "text": "BBT"  },
+        { "url": "http://anidb.net/perl-bin/animedb.pl?show=mylist&do.filter=1",    "text": "ADB"  },
+        { "url": "http://www.mangaupdates.com/mylist.html",                         "text": "MU"   },
+        { "url": "http://vgmdb.net/",                                               "text": "VGM"  },
+        { "url": "https://kissmanga.com",                                             "text": "KM"   },
+        { "url": "http://www.last.fm/home",                                         "text": "l.fm" },
+        { "url": "http://what.cd/index.php",                                        "text": "W.CD" },
+    ]
+}, d = document;
+
+d.addEventListener("DOMContentLoaded", function()
+{
+    d.removeEventListener("DOMContentLoaded", arguments.callee, false);
+
+    for (var i = 0, MAX = config.sites.length; i < MAX; ++i)
+    {
+        var p = i / 12,
+            site = d.createElement("a");
+        site.href = config.sites[i].url;
+        site.textContent = config.sites[i].text;
+
+        if (i % 12 === 0)
+        {
+            var page = d.createElement("div");
+            page.id = "page" + p;
+
+            d.querySelector("#sites").appendChild(page);
+            // d.querySelector("#navigation ul").appendChild((d.createElement("li")));
+
+            if (p === 0)
+            {
+                // d.querySelector("#navigation ul li").classList.add("selected");
+                d.querySelector("#sites > div").classList.add("selected");
+            }
+        }
+
+        d.querySelector("#page" + Math.floor(p)).appendChild(site);
+    }
+});
