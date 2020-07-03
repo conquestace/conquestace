62025f52c1381e2d825fde157f194b4e4a2b2e9c
Ashiful Bhuiyan <ashifulbhuiyan@trentu.ca>
2020-07-03 06:47:56 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -95,3 +95,16 @@
     });
 
 })(jQuery);
+
+<!-- Add a placeholder for the Twitch embed -->
+<div id="twitch-embed"></div>
+
+<!-- Load the Twitch embed script -->
+<script src="https://player.twitch.tv/js/embed/v1.js"></script>
+
+<!-- Create a Twitch.Player object. This will render within the placeholder div -->
+<script type="text/javascript">
+  new Twitch.Player("twitch-embed", {
+    channel: "conquestace"
+  });
+</script>
